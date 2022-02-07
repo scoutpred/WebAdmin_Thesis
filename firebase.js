a8100b9e2b97f0c38f094fc9ca7061d10922c6ed
scoutpred <81546296+scoutpred@users.noreply.github.com>
2022-02-07 12:16:15 +0800

    initial 2

---
+++ firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDtmu1TFuIArTCBJOh_weQwM4O21UNbzFY",
+    authDomain: "webadminthesis.firebaseapp.com",
+    projectId: "webadminthesis",
+    storageBucket: "webadminthesis.appspot.com",
+    messagingSenderId: "995507173149",
+    appId: "1:995507173149:web:38e11a4bc34fa350c053f4"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
